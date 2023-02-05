ce178462d9bf74f9533dfb0e9a856ed39d9af539
ZEAL <tassadiqshakeel5@gmail.com>
2023-02-05 20:51:47 +0500

    create templates

--- Users/urls.py
+++ Users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import first
 
 urlpatterns =[
-    path("<int:id>/<str:name>",first)
+    path("<int:id>",first)
 ]
--- Users/views.py
+++ Users/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import response
 # Create your views here.
 
-def first(request,id, name) -> HttpResponse:
-    return HttpResponse(f"hi {id} {name}")
+
+def first(request,id) -> HttpResponse:
+    context = {
+        "id" : [id]
+    }
+    return render(request,"users/index.html",context)
 
